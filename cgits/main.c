7f9b8b2dd6c91f053b06421f264fbd84433976ae
Mehul Bhujwala <mehuljb@gmail.com>
2013-11-24 00:02:43 +0530

    Initial Commit

---
+++ cgits/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cgits
+//
+//  Created by Mehul Bhujwala on 24/11/2013.
+//  Copyright (c) 2013 Cynx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
